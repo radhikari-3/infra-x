13171132bf61973bc36316836f4a1eab3227dd02
sudhirkumarojhaa <sudhirkumarojhaa@gmail.com>
2023-08-17 00:45:05 +0530

    login

--- src/components/HeaderBox.js
+++ src/components/HeaderBox.js
@@ -1,5 +1,7 @@
-import React from 'react';
-import '../App.css';
+import React from "react";
+import "../App.css";
+import profile from "../assets/profile.avif";
+
 const HeaderBox = ({ title, onLogout }) => {
   const handleLogout = () => {
     onLogout();
@@ -9,8 +11,8 @@ const HeaderBox = ({ title, onLogout }) => {
     <div className="header">
       <div className="header-title">{title}</div>
       <div className="header-profile">
-        <img src={'https://shayari-quotes-status.in/ezoimgfmt/blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhpe7-_576Z2_ByrnziPMtvKwKvzDszknV8CdjMY3EKfSVInNIgnmi9A6gSAYL37hFHTE6PfiArqrA1lO0SDZ8oKZnw3jh0QY4ZVeb8L61d3zkO088fGPhyajAA3w0RuTzbcy4iT7EKAbBXBkH_s50kGxae9FL3jEq3OkSjLBqObORPyE_e9TZxFZthmw/w213-h320/pexels-dreamlens-production-2913125-min.jpg?ezimgfmt=rs:213x320/rscb1/ngcb1/notWebP'} alt="Profile" className="header-profile-image" />
-        <div className="header-logout" >
+        <img src={profile} alt="Profile" className="header-profile-image" />
+        <div className="header-logout">
           <small>James Sudhir</small>
           <small onClick={handleLogout}>Logout</small>
         </div>
--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,15 +1,22 @@
-import React, { useState } from 'react';
-import '../App.css';
-import { FaEye, FaEyeSlash } from 'react-icons/fa';
+import React, { useState } from "react";
+import "../App.css";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
+import logo from "../assets/logo.png";
+import bg from "../assets/bg.webp";
 
 const Login = () => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
   const [showPassword, setShowPassword] = useState(false);
-  const [error, setError] = useState('');
+  const [error, setError] = useState("");
+  const [keepSignedIn, setKeepSignedIn] = useState(false); // New state for checkbox
 
   const handleUsernameChange = (e) => {
     setUsername(e.target.value);
+  };
+
+  const handleKeepSignedInChange = () => {
+    setKeepSignedIn(!keepSignedIn);
   };
 
   const handlePasswordChange = (e) => {
@@ -24,47 +31,63 @@ const Login = () => {
     e.preventDefault();
 
     // Simulated login logic
-    if (username === 'admin' && password === 'password') {
+    if (username === "admin" && password === "password") {
       // Store token in local storage
-      localStorage.setItem('token', 'myToken');
+      localStorage.setItem("token", "myToken");
 
       // Redirect to dashboard page
-      window.location.href = '/chat';
+      window.location.href = "/chat";
     } else {
-      setError('Invalid username or password');
+      setError("Invalid username or password");
     }
   };
 
   return (
-    <div className="login-container">
-      <form className="login-form" onSubmit={handleSubmit}>
-      <h1>Login Page</h1>
-        <div className="form-group">
-          <label htmlFor="username">Username:</label>
-          <input
-            type="text"
-            id="username"
-            value={username}
-            onChange={handleUsernameChange}
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="password">Password:</label>
-          <div className="password-input">
-            <input
-              type={showPassword ? 'text' : 'password'}
-              id="password"
-              value={password}
-              onChange={handlePasswordChange}
-            />
-            <div className="toggle-password" onClick={handleTogglePassword}>
-              {showPassword ? <FaEyeSlash /> : <FaEye />}
+    <div className="box">
+      <img src={logo} alt="logo" />
+      <div className="block">
+        <img src={bg} alt="logo" className="bg" />
+        <div className="login-container">
+          <form className="login-form" onSubmit={handleSubmit}>
+            <h1>Login Page</h1>
+            <div className="form-group">
+              <label htmlFor="username">Username:</label>
+              <input
+                type="text"
+                id="username"
+                value={username}
+                onChange={handleUsernameChange}
+              />
             </div>
-          </div>
+
+            <div className="form-group">
+              <label htmlFor="password">Password:</label>
+              <div className="password-input">
+                <input
+                  type={showPassword ? "text" : "password"}
+                  id="password"
+                  value={password}
+                  onChange={handlePasswordChange}
+                />
+                <div className="toggle-password" onClick={handleTogglePassword}>
+                  {showPassword ? <FaEyeSlash /> : <FaEye />}
+                </div>
+              </div>
+              <div className="block">
+                <input
+                  className="check"
+                  type="checkbox"
+                  checked={keepSignedIn}
+                  onChange={handleKeepSignedInChange}
+                />
+                <small>Keep me logged in</small>
+              </div>
+            </div>
+            <div className="error">{error}</div>
+            <button type="submit">Login</button>
+          </form>
         </div>
-        <div className="error">{error}</div>
-        <button type="submit">Login</button>
-      </form>
+      </div>
     </div>
   );
 };
