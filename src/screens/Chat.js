bc5d9ae9b0cc7e19649fcd6a07ce91e469324bd5
rohit <rohit.adhikari@outlook.com>
2023-08-17 00:43:39 +0530

    uodate

--- src/screens/Chat.js
+++ src/screens/Chat.js
@@ -41,7 +41,78 @@ function Chat() {
             break;
           case 'provide an actual ec2 instance terraform template':
             newReceiverMessage = {
-              content: 'Sure, here is the Terraform code for an EC2 instance:\n... (template)',
+              content: `Sure! Here's an example of how you can create an EC2 instance template using Terraform for AWS:
+              
+              1. Install Terraform:
+              Make sure you have Terraform installed on your machine. You can download it from the official Terraform website: https://www.terraform.io/downloads.html
+
+              2. Create a Directory:
+              Create a new directory for your Terraform configuration files.
+
+              3. Create a Terraform Configuration File (e.g., main.tf):
+              In your directory, create a file named main.tf and add the following content to create an EC2 instance template.
+              
+              \`\`\`hcl
+              provider "aws" {
+                region = "us-west-2"  # Replace with your desired AWS region
+              }
+              
+              resource "aws_instance" "example" {
+                ami           = "ami-12345678"  # Replace with your desired AMI ID
+                instance_type = "t2.micro"
+                subnet_id     = "subnet-12345678"  # Replace with your desired subnet ID
+              
+                tags = {
+                  Name = "ExampleInstance"
+                }
+              }
+              
+              resource "aws_launch_template" "example" {
+                name = "example-template"
+                
+                image_id = aws_instance.example.ami
+                instance_type = "t2.micro"
+                
+                block_device_mappings {
+                  device_name = "/dev/sda1"
+                  ebs {
+                    volume_size = 30
+                  }
+                }
+                
+                tag_specifications {
+                  resource_type = "instance"
+                  tags = {
+                    Name = "ExampleTemplateInstance"
+                  }
+                }
+              }
+              
+              \`\`\`
+              
+              1. Initialize Terraform:
+              Open a terminal in your directory and run the following command to initialize Terraform:
+              \`\`\`sh
+              terraform init
+              \`\`\`
+
+              2. Preview Changes:
+              Run the following command to see the changes that Terraform will make:
+              \`\`\`sh
+              terraform plan
+              \`\`\`
+
+              3. Apply Changes:
+              If the plan looks good, apply the changes:
+              \`\`\`sh
+              terraform apply
+              \`\`\`
+              
+              Confirm the changes when prompted.
+
+              Terraform will create an EC2 instance and an instance launch template according to the configuration you specified in the main.tf file. Make sure to replace placeholder values like AMI ID, subnet ID, and region with appropriate values for your AWS environment.
+
+              Always refer to the official Terraform documentation and AWS documentation for the most accurate and up-to-date information.`,
               sender: 'receiver',
             };
             break;
